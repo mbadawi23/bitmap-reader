ddc92ac0734ec2dfe6876a1b80d052695036a10a
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2016-09-26 11:35:05 -0700

    Initial Commit: - Added a static method that returns a byte array for a hard-coded file.

--- BitmapByteReader/Program.cs
+++ BitmapByteReader/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace BitmapByteReader
 {
@@ -10,6 +11,9 @@ namespace BitmapByteReader
     {
         static void Main(string[] args)
         {
+            Image image = Image.FromFile(@"C:\MyWorkspace\media\dotmatrix.bmp");
+
+            byte[] bytes = BitmapReader.ImageToByte(image);
         }
     }
 }
